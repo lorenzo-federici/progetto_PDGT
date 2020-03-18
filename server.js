fe9966abc27d93fc8601db75c6fecd13662d2893
Lorenzo <federici.l30@gmial.com>
2020-03-18 18:09:50 +0100

    miglioramento file server.js
    aggiunta di un messaggio  a terminale nel momento in cui parte il server

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const port = process.env.port || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () =>{
+    console.log("Server started on port: " + port);
+});
