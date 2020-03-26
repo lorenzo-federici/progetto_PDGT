ec54abeac89fc4ecabe04f415f752ee2fad5561c
Lorenzo <federici.l30@gmial.com>
2020-03-26 12:42:29 +0100

    piccolo miglioramento sulle richieste get
    ho differenziato la richiesta di visualizzare le stazioni per nome/id/provincia/regione con la richiesta della stazione piu vicino

--- api/routes/stations.js
+++ api/routes/stations.js
@@ -8,13 +8,11 @@ const StationController = require('../controllers/stations_ctrl');
 
 
 //-------------------------------------------------------------
-router.get('/:option', StationController.stations_get_stations);
+router.get('/view/:option', StationController.stations_get_stations);
+router.get('/near/', StationController.stations_get_one_near);
 router.post('/', checkAuth, StationController.stations_add_stations);
 router.delete('/:stationId', checkAuth, StationController.stations_delete_station);
 router.patch('/:stationId', checkAuth, StationController.stations_update_stations);
-
-//router.get('/near/:latitude/:longitude', StationController.stations_get_one_near);
-router.get('/near', StationController.stations_get_one_near);
 //-------------------------------------------------------------
 
 module.exports = router;
