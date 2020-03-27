51e6c76bd04414e11383752571d9ceac90735eb9
Lorenzo <federici.l30@gmial.com>
2020-03-27 12:38:53 +0100

    aggiunto il controllo del token sulla visualizzazione di tutti gli utenti

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,7 +7,7 @@ const UserController = require("../controllers/users_ctrl"); //user_ctrl
 //-------------------------------------------------------------
 
 //-------------------------------------------------------------
-router.get('/', UserController.users_get_all);
+router.get('/', checkAuth, UserController.users_get_all);
 router.post('/signup', UserController.users_signup);
 router.post('/login', UserController.users_login);
 router.delete('/:userId', checkAuth, UserController.users_delete_user);
